64e85f4d0d3a1fd51118e02d85a5a7b85c1a59ec
Dicksen Tan <tandicksen@gmail.com>
2024-10-21 20:42:16 +0700

    Create: Refresh Token and controllers
    Refresh Token is used so the main token wont last long and can be generated aslong rrefresh token still available

--- controllers/userControlller.js
+++ controllers/userControlller.js
@@ -4,13 +4,68 @@ const jwt = require('jsonwebtoken')
 
 //to create jwt
 const createToken = (id) =>{
-    return jwt.sign({id}, process.env.SECRET, {expiresIn: '1d'})
+    return jwt.sign({id}, process.env.SECRET, {expiresIn: '15m'})
+}
+const createRefreshToken = (id) =>{
+    return jwt.sign({id}, process.env.REFRESH_SECRET,{expiresIn: '7d'})
 }
 
-const getData = async(req,res) =>{
-    const {id} = req.params
+const userSignUp = async(req,res) =>{
+    
+    const {email, password} = req.body
     try{
-        const user = new User.findById(id)
+        const newuser = await User.signUp(email, password)
+        const token = createToken(newuser._id)
+        res.cookie('token', token,{
+            httpOnly:true,
+            secure:process.env.NODE_ENV === 'production',
+            maxAge:360000,
+            sameSite:'strict'
+        })
+        const refreshToken = createRefreshToken(newuser._id)
+        res.cookie('refreshToken', refreshToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            sameSite: 'strict'
+        })
+        return res.status(200).json(email)
+    }catch(error){
+        return res.status(400).json(error)
+    }
+}
+
+const userLogin = async(req,res) =>{
+    const {email, password} = req.body
+    try{
+        const user = User.userLogin(email,password)
+        const token = createToken(user._id)
+        res.cookie('token', token,{
+            httpOnly:true,
+            secure:process.env.NODE_ENV === 'production',
+            maxAge:360000,
+            sameSite:'strict'
+        })
+        const refreshToken = createRefreshToken(user._id)
+        res.cookie('refreshToken', refreshToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            sameSite: 'strict'
+        })
+        return res.status(200).json(email)
+    }catch(error){
+        return res.status(400).json(error)
+    }
+}
+
+//Controller below need auth
+
+//get data
+const getData = async(req,res) =>{
+    const userId = req.user.id
+    try{
+        const user = new User.findById(userId)
         .populate({
             path:'boards',
             populate:{
@@ -23,34 +78,45 @@ const getData = async(req,res) =>{
                 }
             }
         })
-        res.status(200).json(user)
+        return res.status(200).json(user)
     }catch(error){
-        res.status(400).json({error: 'Error fetch', error})
+        return res.status(400).json({error: 'Error fetch', error})
     }
 }
+//refresh token
+const refreshToken = (req,res)=>{
+    const checkToken = req.cookie.refreshToken
+    if(!checkToken){
+        return res.status(401).json({mssg: 'Token Expired'})
+    }
+    const verify = jwt.verify(refreshToken, process.env.REFRESH_SECRET)
+    if(!verify){
+        return res.status(401).json({mssg:'Invalid Refresh Token'})
+    }
 
-const userSignUp = async(req,res) =>{
-    const {email, password} = req.body
+    //aslong refresh token still active, generate newToken
+    const newToken = createToken(verify)
+    // replacing
+    res.cookie('token', newToken, {
+        httpOnly:true,
+        secure:process.env.NODE_ENV === 'production',
+        maxAge: 15 * 60 * 1000,
+        sameSite: 'strict'
+    })
+}
 
+const loginCheck = async(req,res) =>{
+    const token = req.cookies.token
+    if(!token){
+        return res.status(400).json('Token Expired')
+    }
     try{
-        const newuser = await User.signUp(email, password)
-        const token = createToken(newuser._id)
-        res.status(200).json({email, token})
+        const decode = jwt.verify(token, process.env.SECRET)
+        const user = await User.find({_id:decode.id})
+        return res.status(200).json({email: user.email})
     }catch(error){
-        res.status(400).json(error)
+        return res.status(401).json(error)
     }
 }
 
-const userLogin = async(req,res) =>{
-    const {email, password} = req.body
-
-    try{
-        const user = User.userLogin(email,password)
-        const token = createToken(user._id)
-        res.status(200).json(user, token)
-    }catch(error){
-        res.status(400).json(error)
-    }
-}
-
-module.exports = {getData, userSignUp, userLogin}
+module.exports = {refreshToken, userSignUp, userLogin, loginCheck, getData}
