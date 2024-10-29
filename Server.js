936a3a10f2b632d9cd8e8c13b3d10c4d4aa2e27b
Dicksen Tan <tandicksen@gmail.com>
2024-10-29 12:09:05 +0700

    Add: Cookie parser

--- Server.js
+++ Server.js
@@ -3,9 +3,10 @@ const cors = require('cors')
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-
+const cookieParser = require('cookie-parser')
 
 app.use(express.json())
+app.use(cookieParser())
 app.use(cors({
     origin: "http://localhost:5173",
     credentials:true
