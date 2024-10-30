a4b93dd0d2b9634a1fc4db80757e2db7d53e9f97
Dicksen Tan <tandicksen@gmail.com>
2024-10-30 20:27:53 +0700

    Move: refresh token routes

--- routes/userAuthRoutes.js
+++ routes/userAuthRoutes.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const {loginCheck, refreshToken, getData} = require('../controllers/userControlller')
+const {loginCheck, getData} = require('../controllers/userControlller')
 const auth = require('../middleware/requireAuth')
 
 router.use(auth)
 router.get('/loginCheck', loginCheck)
-router.get('/refreshToken', refreshToken)
 router.get('/getData', getData)
 
 module.exports = router
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const {userSignUp, userLogin} = require('../controllers/userControlller')
+const {userSignUp, userLogin, refreshToken} = require('../controllers/userControlller')
 
 router.post('/login', userLogin)
 router.post('/signup', userSignUp)
-
+router.get('/refreshToken', refreshToken)
 module.exports = router
